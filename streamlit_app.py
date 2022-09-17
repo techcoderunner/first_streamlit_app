2887e07a5a34389cde46d1cd1fee011411ded503
techcoderunner <londhesachin6@gmail.com>
2022-09-17 11:20:59 +0530

    added extra texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new healthy dinner')
+
+streamlit.header("Breakfast Menu")
+
+streamlit.text("Omega 3 and Blueberry oatmeal")
+
+streamlit.text("Kale, SPinach and Rocket Smothy")
